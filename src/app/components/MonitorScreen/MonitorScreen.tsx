43bbcd738956b7d48ff870dd12e611d607be371d
alessandrobattiato <alessandroDev@outlook.it>
2025-02-05 17:58:29 +0100

    feat: first render of HTML inside of MonitorScreen

--- src/app/components/MonitorScreen/MonitorScreen.tsx
+++ src/app/components/MonitorScreen/MonitorScreen.tsx
@@ -1,47 +1,20 @@
 "use client";
-import { useFBO } from "@react-three/drei";
-import { useFrame, createPortal } from "@react-three/fiber";
-import { useRef } from "react";
+import { Html } from "@react-three/drei";
 import { MonitorScreenProps } from "./types";
 
-const MonitorScreen = ({ children, isInsideMonitor }: MonitorScreenProps) => {
-    const fbo = useFBO(1024, 1024);
-    const virtualScene = useRef(null);
-    const camera = useRef(null);
-
-    useFrame((state) => {
-        if (virtualScene.current && camera.current) {
-            state.gl.setRenderTarget(fbo);
-            state.gl.render(virtualScene.current, camera.current);
-            state.gl.setRenderTarget(null);
-        }
-    });
-
-    if (!virtualScene.current) {
-        return null;
-    }
-
+const MonitorScreen = ({ isInsideMonitor, children }: MonitorScreenProps) => {
     return (
-        <>
-            {createPortal(
-                <scene ref={virtualScene}>
-                    <perspectiveCamera ref={camera} position={[0, 0, 5]} />
-                    <group
-                    // style={{
-                    //     pointerEvents: isInsideMonitor ? "auto" : "none",
-                    // }}
-                    >
-                        {children}
-                    </group>
-                </scene>,
-                virtualScene.current || document.body
-            )}
-
-            <mesh position={[0, 1, 0]}>
-                <planeGeometry args={[1.5, 1]} />
-                <meshBasicMaterial map={fbo.texture} />
-            </mesh>
-        </>
+        <Html
+            transform
+            occlude
+            rotation-x={-0.175}
+            position={[0, -1.525, -9.85]}
+            style={{
+                pointerEvents: isInsideMonitor ? "auto" : "none",
+            }}
+        >
+            {children}
+        </Html>
     );
 };
 
--- src/app/components/Portfolio/Portfolio.tsx
+++ src/app/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Portfolio = () => {
-    return <div>Portfolio</div>;
+    return (
+        <div className="bg-red-500 rounded-tr rounded-tl h-[125px] w-52">
+            Portfolio
+        </div>
+    );
 };
 
 export default Portfolio;
