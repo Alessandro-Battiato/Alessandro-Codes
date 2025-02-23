98b6cb379ea8c77608ee46de99cca48763ae0da8
alessandrobattiato <alessandroDev@outlook.it>
2025-02-23 17:50:04 +0100

    feat: replaces astronaut mouse and macbook models with a single merged model

--- src/app/hooks/useGLTFModels/useGLTFModels.ts
+++ src/app/hooks/useGLTFModels/useGLTFModels.ts
@@ -3,5 +3,17 @@ import { useGLTF } from "@react-three/drei";
 export const useGLTFModels = () => {
     const { scene: monitor } = useGLTF("/models/Monitor/Monitor.glb");
     const { scene: desk } = useGLTF("/models/Desk/scene.gltf");
-    return { monitor, desk };
+    const { scene: astronaut, animations } = useGLTF(
+        "/models/Astronaut/MergedModels.glb"
+    );
+
+    return {
+        monitor,
+        desk,
+        astronautObj: { astronaut, animations },
+    };
 };
+
+useGLTF.preload("/models/Monitor/Monitor.glb");
+useGLTF.preload("/models/Desk/scene.gltf");
+useGLTF.preload("/models/Astronaut/MergedModels.glb");
