b616a271d6684e924070453b40f2318ff4308d17
alessandrobattiato <alessandroDev@outlook.it>
2025-03-06 21:15:51 +0100

    feat: adds colors palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "dark-space": "#0A0527",
+                "electric-blue": "#6B6CFF",
+                "pastel-violet": "#8B5CF6",
+                "intense-pink": "#FF66B2",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
