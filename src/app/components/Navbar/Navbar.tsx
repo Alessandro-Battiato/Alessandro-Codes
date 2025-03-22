2cfe5dde41388834d3d8b97742b721572571b3eb
alessandrobattiato <alessandroDev@outlook.it>
2025-03-22 12:22:12 +0100

    fix: changes navbar links' background color

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar() {
                                     {hoveredNavItem === index && (
                                         <motion.div
                                             layoutId={id}
-                                            className="absolute inset-0 -z-10 bg-[#0D1B2A]"
+                                            className="absolute inset-0 -z-10 bg-gradient-to-r bg-[#1E2A47]"
                                             initial={{ borderRadius: 8 }}
                                         />
                                     )}
