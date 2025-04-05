91a9ea2f8c00c20076c7b14a5b375f251d6395a3
alessandrobattiato <alessandroDev@outlook.it>
2025-04-05 14:15:27 +0200

    feat: isolates custom utility hook for window sizes

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useCameraController } from "./useCameraController/useCameraController";
 export { useGLTFModels } from "./useGLTFModels/useGLTFModels";
+export { useWindowSize } from "./useWindowSize/useWindowSize";
 export { useReducedMotion } from "./useReducedMotion/useReducedMotion";
 export { useMouseScrollHandler } from "./useMouseScrollHandler/useMouseScrollHandler";
 export { useScrollProgress } from "./useScrollProgress/useScrollProgress";
