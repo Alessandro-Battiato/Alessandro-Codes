60153220c793d411a857754bda61b7f3284703f9
alessandrobattiato <alessandroDev@outlook.it>
2025-04-06 19:09:33 +0200

    fix: small footer style fix

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <section className="p-6 bg-dark-space text-gray-200">
+        <section className="p-8 bg-dark-space text-gray-200">
             <footer className="text-center">
                 <p className="text-sm">
                     © 2025 AlessandroCodes, All rights reserved
