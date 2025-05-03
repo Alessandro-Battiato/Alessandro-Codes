d00d0db79c8c480f6a0317fc38b07821e13c6c7c
alessandrobattiato <alessandroDev@outlook.it>
2025-05-03 12:27:37 +0200

    refactor: extract navbar and footer out of main tag to better align with HTML semantics

--- src/components/ui/Portfolio/Portfolio.tsx
+++ src/components/ui/Portfolio/Portfolio.tsx
@@ -27,48 +27,52 @@ const Portfolio = () => {
     const contactMeRef = useRef<HTMLDivElement | null>(null);
 
     return (
-        <main
-            ref={containerRef}
-            className="min-h-screen relative -z-10 bg-dark-space w-full"
-        >
-            <Canvas
-                eventSource={containerRef as React.RefObject<HTMLElement>}
-                className="!fixed z-10"
+        <>
+            <header>
+                <Navbar />
+            </header>
+
+            <main
+                ref={containerRef}
+                className="min-h-screen relative -z-10 bg-dark-space w-full"
             >
-                <View track={heroRef as React.RefObject<HTMLElement>}>
-                    <Hero3D />
-                </View>
-                <View track={aboutMeRef as React.RefObject<HTMLElement>}>
-                    <AboutMe3D />
-                </View>
-                <View track={contactMeRef as React.RefObject<HTMLElement>}>
-                    <ContactMe3D />
-                </View>
-            </Canvas>
+                <Canvas
+                    eventSource={containerRef as React.RefObject<HTMLElement>}
+                    className="!fixed z-10"
+                >
+                    <View track={heroRef as React.RefObject<HTMLElement>}>
+                        <Hero3D />
+                    </View>
+                    <View track={aboutMeRef as React.RefObject<HTMLElement>}>
+                        <AboutMe3D />
+                    </View>
+                    <View track={contactMeRef as React.RefObject<HTMLElement>}>
+                        <ContactMe3D />
+                    </View>
+                </Canvas>
 
-            <div className="fixed inset-0 -z-20 w-full h-full pointer-events-none">
-                <Image
-                    src="/assets/background.svg"
-                    alt="background image"
-                    className="blur-[100px]"
-                    fill
-                />
-            </div>
+                <div className="fixed inset-0 -z-20 w-full h-full pointer-events-none">
+                    <Image
+                        src="/assets/background.svg"
+                        alt="background image"
+                        className="blur-[100px]"
+                        fill
+                    />
+                </div>
 
-            <Navbar />
+                <Hero ref={heroRef} />
 
-            <Hero ref={heroRef} />
+                <AboutMe ref={aboutMeRef} />
 
-            <AboutMe ref={aboutMeRef} />
+                <Projects />
 
-            <Projects />
+                <Blog />
 
-            <Blog />
-
-            <ContactMe ref={contactMeRef} />
+                <ContactMe ref={contactMeRef} />
+            </main>
 
             <Footer />
-        </main>
+        </>
     );
 };
 
